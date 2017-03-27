80d020e2ea68778245cc9c0d638384cc53048d7b
Juviee <cuso43k@gmail.com>
2017-03-27 06:57:20 +0300

    Homework: Deep cloning

---
+++ 3d_shot/deep_clone.pm
@@ -0,0 +1,54 @@
+use Data::Dumper;
+use strict;
+
+
+sub refer_recursor{
+    #get some reference
+    #return copy of structure under that refera
+    my $arg = shift;
+
+#    print ref $arg,"\n";
+    if(!(ref $arg cmp "ARRAY")){
+        my @rtn;
+        print "Array is", @$arg, "\n";
+        while(my($k, $v) = each @$arg){
+            $rtn[$k] = refer_recursor($v);
+        }
+        return \@rtn;
+    }
+    elsif(!(ref $arg cmp "HASH")){
+        print "HSH\n";
+        my %rtn;
+        while(my ($k, $v) = each %$arg){
+            $rtn{$k} = refer_recursor($v);
+        }
+        return \%rtn;
+    }
+    elsif(!(ref $arg cmp "")){#scalar string,num or undef
+        return $arg;
+    }
+    else{#ref on scalar, or ref, or func, for exmpl. i wont print them, gimmi money
+        print "undefined behavior\n";
+
+    }
+
+}
+my $val = 42;
+my $ref = \$val;
+my $foo = [
+  {
+      "a", 1,
+      "b", 2
+  },
+  {
+      'c', 3,
+      'd', {
+          "e", 4,
+          "f", undef() 
+      }
+  },
+  "Cockroach pain",
+  $ref
+];
+my $bar = refer_recursor($foo);
+print Dumper($bar);
